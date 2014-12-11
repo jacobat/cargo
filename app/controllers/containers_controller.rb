89946de442e15f2d8d7375126d50f3ed147834de
Sune Fengel <fengel@gmail.com>
2014-12-11 17:15:43 +0100

    Refactoring and implementation of images

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,8 +1,10 @@
 class ContainersController < ApplicationController
   def index
+    @containers = Host.new(DOCKER_ENDPOINT).containers
   end
 
   def show
+    @containers = Host.new(DOCKER_ENDPOINT).containers
     @container = Container.new(DOCKER_ENDPOINT, params[:id])
   end
 end
