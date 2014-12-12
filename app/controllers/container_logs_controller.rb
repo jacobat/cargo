f72cb546639994fc521543c3b206f4b1822d6ebc
Jacob Atzen <jatzen@gmail.com>
2014-12-12 10:32:11 +0100

    Live reloading logs

--- app/controllers/container_logs_controller.rb
+++ app/controllers/container_logs_controller.rb
@@ -1,6 +1,21 @@
-class ContainerLogsController < ApplicationController
+require 'reloader/sse'
+class ContainerLogsController < ActionController::Base
+  include ActionController::Live
+
   def show
-    @container = Container.new(DOCKER_ENDPOINT, params[:id])
+    container = Container.new(DOCKER_ENDPOINT, params[:id])
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = Reloader::SSE.new(response.stream)
+
+    container.tunnel_logs do |chunk|
+      sse.write({ line: chunk }, { event: 'log' })
+    end
+  rescue IOError => ex
+  rescue Exception => ex
+    puts "Exception: #{ex.message}"
+    puts ex.backtrace
+  ensure
+    response.stream.close
   end
 end
 
